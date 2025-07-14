4770d1ac1eac4fe4082e934afa6ba45262411952
yasin <yasin.xu@icloud.com>
2025-07-15 00:29:45 +0800

    transparent

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,17 @@
 return {
-  { "rose-pine/neovim" },
+  -- { "rose-pine/neovim" },
 
   -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight-night",
-    },
+
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "tokyonight-night",
+  --   },
+  -- },
+  "folke/tokyonight.nvim",
+  opts = {
+    style = "night",
+    transparent = true,
   },
 }
