e413c8fc3fd141c3158f85b0df855cb966e52b35
yasin <yasin.xu@bufflogistics.com>
2025-07-17 21:15:04 +0800

    spaces

--- sketchybar/items/aerospace_space.sh
+++ sketchybar/items/aerospace_space.sh
@@ -1,40 +1,19 @@
 #!/bin/bash
 
-workspaces=$(aerospace list-workspaces --all)
+space_item=(
+  padding_left=0
+  padding_right=0
+  icon.drawing=off
+  label.drawing=off
+  script="$PLUGIN_DIR/aerospace_space.sh"
+  label.font="sketchybar-app-font:Regular:14.0"
+)
 
-for space in $workspaces; do
+for space in $(aerospace list-workspaces --all); do
 
-  windows="$(aerospace list-windows --workspace "$space")"
-
-  if [ -z "$windows" ]; then
-    :
-  else
-    # 移除已存在的 workspace item，避免重复
-    sketchybar --remove "$space" 2>/dev/null
-
-    sketchybar --add item "$space" left \
-               --set "$space" label="$space"
-
-    echo "$windows" | while read -r window; do
-      appName=$(echo "$window" | awk -F '|' '{print $2}' | sed 's/^ *//;s/ *$//')
-      window_id=$(echo "$window" | awk -F '|' '{print $1}' | sed 's/^ *//;s/ *$//')
-      item_name="space_${space}_window_${window_id}"
-
-      icon="$($CONFIG_DIR/plugins/icon_map.sh "$appName")"
-
-      app=(
-        icon.font="sketchybar-app-font:Regular:14.0"
-        icon="$icon"
-        label.drawing=off
-        padding_left=0
-        padding_right=0
-      )
-
-      sketchybar --remove "$item_name" 2>/dev/null
-
-      sketchybar --add item "$item_name" left \
-                 --set "$item_name" "${app[@]}"
-    done
-  fi
-
+  item_name="space::$space"
+  sketchybar  --add item "$item_name" left \
+              --set "$item_name" "${space_item[@]}" \
+              --subscribe "$item_name" aerospace_workspace_change
 done
+
